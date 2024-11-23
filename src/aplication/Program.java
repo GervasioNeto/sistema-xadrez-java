c406c1b7e36f02445ed0c0206eb4035023a1cda4
Gervasio Neto <gervasiolopesn@gmail.com>
2024-11-23 12:37:33 -0300

    primeira classe Position

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,2 +1,11 @@
-package aplication;public class Program {
+package aplication;
+
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Position p = new Position(3,5);
+        System.out.println(p);
+    }
 }
