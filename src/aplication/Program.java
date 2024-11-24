af92f1dbfbc04853f7aa45a24f300c28ab898c45
Gervasio Neto <gervasiolopesn@gmail.com>
2024-11-24 13:17:55 -0300

    camada Chess e print Board

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,14 +2,12 @@ package aplication;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
 
-        Position p = new Position(3,5);
-        System.out.println(p);
-
-        Board b = new Board(8,8);
-        System.out.println(b);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
