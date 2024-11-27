df7d2b9e33751e8bc60936cdafbda373ba5fc222
Gervasio Neto <gervasiolopesn@gmail.com>
2024-11-27 12:10:30 -0300

    movimentos possiveis

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -42,7 +42,9 @@ public class ChessMatch {
 
     private void validateSourcePosition(Position position){
         if(!board.thereIsAPiece(position)){
-            throw new ChessException("Nao existe peca na prosicao de origem");
+            throw new ChessException("Nao existe peca na posicao de origem");
+        } if (!board.piece(position).isThereAnyPossibleMove()){
+            throw new ChessException("Nao existe movimentos possiveis para a peca escolhida");
         }
     }
 
